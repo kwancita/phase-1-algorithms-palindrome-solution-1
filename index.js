ef1a6736d00b6aa4dce34b83ae0cbba25faa5384
Wasanta <kwan_u_day@hotmail.com>
2022-01-16 15:08:12 -0500

    done

--- index.js
+++ index.js
@@ -1,9 +1,55 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+/*write the function to reverse the word in new string 
+if after reverse the word to new string and 
+they still read in the same way return ture if not return false*/
+function reverse(word){
+  return word.split("").reverse().join("");
+  // const splitWordArray = word.split("")
+  // const reverseWordArray = splitWordArray.reverse()
+  // const joinWordArray = reverseWordArray.join("")
+  // return joinWordArray
 }
+function isPalindrome(word){
+  const reverseWord = reverse(word)
+  return reverseWord === word
+  // if(reverseWord === word){
+  //   return true
+  // }else{
+  //   return false
+  // }
+}
+
+
+//kwan's solution
+// function isPalindrome(word) {
+//   // Write your algorithm here
+//   // set newStr to empty
+//   var newStr = ""
+//   // iterate over the word
+//   for (var i = word.length-1; i>=0; i--){
+//   //   store index of word in newStr
+//     newStr += word[i]
+//   }
+//   // if newStr === word
+//   if (newStr === word){
+//   //   return true
+//     return true
+//     // else
+//   }else{
+//   //   return false
+//     return false
+//   }
+// }
 
 /* 
   Add your pseudocode here
+
+  set newStr to empty
+  iterate over the word
+    store index of word in newStr
+  if newStr === word
+    return true
+  else
+    return false
 */
 
 /*
@@ -20,6 +66,9 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
 }
 
 module.exports = isPalindrome;
